f32f7b5b6e2e709f4f1a5c27f20aeb34185cb212
Stephen Wagstaff <srwagsta@gmail.com>
2018-11-14 15:07:35 -0600

    Merge sort with pattern matching is all done

--- Homework/Homework9/Solutions/homework9_readme.md
+++ Homework/Homework9/Solutions/homework9_readme.md
@@ -11,7 +11,31 @@ Homework 9: Sorting Algorithms in Scala
 a list, and return a sorted list in **increasing order**.
 
     ```scala
-    def merge_sort(lst: List[Int]): List[Int] =
+      def merge_sort(initialList: List[Int]): List[Int] = {
+    
+        def split: (List[Int], List[Int]) = {
+          val cut = initialList.length/2
+          ( initialList take cut , initialList drop cut )
+        }
+    
+        def merge(leftList: List[Int], rightList: List[Int]): List[Int] = {
+          (leftList, rightList) match {
+            case (Nil, _) => rightList
+            case (_, Nil) => leftList
+            case ((leftElement::leftRemaining), (rightElement::rightRemaining)) =>
+              if (leftElement > rightElement) leftElement::merge(leftRemaining, rightList)
+              else rightElement::merge(leftList, rightRemaining)
+          }
+        }
+    
+        initialList match{
+          case Nil => Nil
+          case x::Nil => x::Nil
+          case _ =>
+            val (left, right) = split
+            merge (merge_sort(left), merge_sort(right))
+        }
+      }
     ```
 
 2. Write a function `selection_sort(lst: List[Int]): List[Int]` that
--- Scala/Homework9.scala
+++ Scala/Homework9.scala
@@ -1,37 +1,46 @@
 object Homework9 {
 
-  def merge_sort(lst: List[Int]): List[Int] = {
-    def split (): (List[Int], List[Int]) = {
-      val cut = lst.length/2
+  def merge_sort(initialList: List[Int]): List[Int] = {
 
-      // TODO: Slice the lists and return the results
-      ( List.take (list,cut) , List.drop (list,cut) )
-
-
+    def split: (List[Int], List[Int]) = {
+      val cut = initialList.length/2
+      ( initialList take cut , initialList drop cut )
     }
 
-    def merge(list1: List[Int], list2: List[Int]): List[Int] = {
-      0
-      // TODO: Implement this
+    def merge(leftList: List[Int], rightList: List[Int]): List[Int] = {
+      (leftList, rightList) match {
+        case (Nil, _) => rightList
+        case (_, Nil) => leftList
+        case ((leftElement::leftRemaining), (rightElement::rightRemaining)) =>
+          if (leftElement > rightElement) leftElement::merge(leftRemaining, rightList)
+          else rightElement::merge(leftList, rightRemaining)
+      }
     }
 
-
-
+    initialList match{
+      case Nil => Nil
+      case x::Nil => x::Nil
+      case _ =>
+        val (left, right) = split
+        merge (merge_sort(left), merge_sort(right))
+    }
   }
 
   def selection_sort(lst: List[Int]): List[Int] = {
     0
   }
 
-  def insertion_sort(lst: List[Int]): List[Int] = {
 
-  }
+//
+//  def insertion_sort(lst: List[Int]): List[Int] = {
+//
+//  }
 
 
   def main(args: Array[String]) {
     val lst = List(5,4,11,2,3,1,0,9)
     println(merge_sort(lst))
     println(selection_sort(lst))
-    println(insertion_sort(lst))
+//    println(insertion_sort(lst))
   }
 }
