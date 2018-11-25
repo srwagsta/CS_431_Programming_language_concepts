b2bcf1dc4b9027f88a41793bfa4e7647522d56bb
Stephen Wagstaff <srwagsta@gmail.com>
2018-11-24 22:08:07 -0600

    REFACTOR! Updated documentation directory to have a single docker container that will find all the markdown documentation files and create the PDF output in a unified directory. WIth that all other docker structure files releating to pdf output has been removed along with all previous pdf output. Homework 11 documentation and scala object structure has now been added.

---
+++ Homework/Homework11/Solutions/homework11_readme.md
@@ -0,0 +1,9 @@
+Homework 11: Derivation of arithmetic expressions in Scala 
+==========================================================
+
+*Stephen Wagstaff* \
+*CS 431* \
+*November 24, 2018*
+
+---
+
---
+++ Scala/Homework11.scala
@@ -0,0 +1,18 @@
+object Homework11
+{
+
+
+  def main(args: Array[String]) {
+//    val e = Times (Times (Var("x"), Var("y")), Plus (Var("x"), Const(3)))
+//    val e1 = Pow (Var("x"), 4)
+//    println(e)
+//    println(e1)
+//    println(e.deriv("x"))
+//    println(e1.deriv("x"))
+//    println(e.deriv("x").simplify)
+//    println(e1.deriv("x").simplify)
+//    val e2 = Pow (Plus (Var("x"), Const(0)), 2)
+//    println(e2)
+//    println(e2.simplify)
+  }
+}
